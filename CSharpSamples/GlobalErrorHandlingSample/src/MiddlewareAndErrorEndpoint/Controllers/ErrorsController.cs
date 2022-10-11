4c84431b4dcc2986692a8585c2810ebfcf019b0d
Saman Qaydi (Lap01) <Saman.Qaydi@Gmail.com>
2022-10-11 14:06:40 +0200

    Added Middleware and ErrorEndpoint Sample

---
+++ CSharpSamples/GlobalErrorHandlingSample/src/MiddlewareAndErrorEndpoint/Controllers/ErrorsController.cs
@@ -0,0 +1,17 @@
+﻿namespace MiddlewareAndErrorEndpoint.Controllers;
+
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[ApiExplorerSettings(IgnoreApi = true)] // We add this because if we define an action without http method attribute, swagger will not run properly
+public class ErrorsController : ControllerBase
+{
+    // No Http Method
+    [Route("/error")]
+    public IActionResult Error()
+    {
+        var exception = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+        return Problem(title: exception.Message);
+    }
+}
---
+++ CSharpSamples/GlobalErrorHandlingSample/src/MiddlewareAndErrorEndpoint/Models/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace MiddlewareAndErrorEndpoint.Models;
+
+public class Student
+{
+    public long Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+}
