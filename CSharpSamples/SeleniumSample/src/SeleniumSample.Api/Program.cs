321b35cf6077d929671960f02206d3e0527ed9dc
Saman Qaydi (Lap01) <Saman.Qaydi@Gmail.com>
2022-11-12 21:23:11 +0100

    Updated ChromeDriver Register in C# selenium Sample

--- CSharpSamples/SeleniumSample/src/SeleniumSample.Api/Program.cs
+++ CSharpSamples/SeleniumSample/src/SeleniumSample.Api/Program.cs
@@ -3,6 +3,12 @@ using OpenQA.Selenium.Chrome;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//ChromeOptions _chromeOptions = new ChromeOptions();
+//_chromeOptions.AddArgument("--incognito");
+//_chromeOptions.AddArgument("--disable-gpu");
+//_chromeOptions.AcceptInsecureCertificates = true;
+//builder.Services.AddScoped<IWebDriver>(w => new ChromeDriver(_chromeOptions));
+
 builder.Services.AddScoped<IWebDriver, ChromeDriver>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
