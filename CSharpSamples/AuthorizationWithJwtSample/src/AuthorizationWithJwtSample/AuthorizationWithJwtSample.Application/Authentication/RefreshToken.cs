b5cd7f1fc9d042b7aad21f94d16f3fbad599ab15
Saman Qaydi (Lap01) <Saman.Qaydi@Gmail.com>
2022-12-07 16:36:34 +0100

    AuthorizationWithJwtSample  - Changed RefreshToken

--- CSharpSamples/AuthorizationWithJwtSample/src/AuthorizationWithJwtSample/AuthorizationWithJwtSample.Application/Authentication/RefreshToken.cs
+++ CSharpSamples/AuthorizationWithJwtSample/src/AuthorizationWithJwtSample/AuthorizationWithJwtSample.Application/Authentication/RefreshToken.cs
@@ -1,5 +1,6 @@
 ﻿namespace AuthorizationWithJwtSample.Application.Authentication;
 
-public record RefreshToken(
-    string Token,
-    DateTime ExipryDateTime);
+public record RefreshToken(string Token, DateTime ExipryDateTime)
+{
+    public DateTime CreateDateTime { get; init; } = DateTime.UtcNow;
+}
