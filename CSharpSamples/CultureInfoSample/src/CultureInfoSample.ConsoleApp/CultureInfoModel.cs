2e9425a95fbf41c5a0bcc05ae79d395a1e218b0f
Saman Qaydi (Pontis) <saman.qaydi@gmail.com>
2022-12-26 18:08:47 +0100

    Added - CultureInfoSample

---
+++ CSharpSamples/CultureInfoSample/src/CultureInfoSample.ConsoleApp/CultureInfoModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CultureInfoSample.ConsoleApp;
+
+public class CultureInfoModel
+{
+    public string Name { get; set; } = string.Empty;
+    public string EnglishName { get; set; } = string.Empty;
+    public string DisplayName { get; set; } = string.Empty;
+    public string ShortDatePattern { get; set; } = string.Empty;
+}
---
+++ CSharpSamples/CultureInfoSample/src/CultureInfoSample.ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using CultureInfoSample.ConsoleApp;
+using Newtonsoft.Json;
+using System.Globalization;
+
+var cultureInfoModels = new List<CultureInfoModel>();
+
+
+var cultures = CultureInfo.GetCultures(CultureTypes.NeutralCultures);
+
+// Getting all cultures.
+foreach (var culterInfo in CultureInfo.GetCultures(CultureTypes.NeutralCultures))
+{
+    cultureInfoModels.Add(
+        new CultureInfoModel
+        {
+            DisplayName = culterInfo.Name,
+            EnglishName = culterInfo.EnglishName,
+            Name = culterInfo.Name,
+            ShortDatePattern = culterInfo.DateTimeFormat.ShortDatePattern
+        });
+}
+
+
+// Converting to json
+var jsonResult = JsonConvert.SerializeObject(cultureInfoModels, Formatting.Indented);
+
+// Saving the result into a file.
+var executingPath = Environment.CurrentDirectory;
+File.WriteAllText(executingPath + "\\jsonResult.json", jsonResult);
+Console.WriteLine(jsonResult);
